1ced92e42a2bf0a3c343dfcfbf8c44ce8450efed
BailGui <guillaumebaillieux88@gmail.com>
2024-04-20 17:19:57 +0200

    adModel

--- model/adminModel.php
+++ model/adminModel.php
@@ -3,4 +3,10 @@
 function connectAdministrator(PDO $co, string $user, string $password) : bool|string 
 {
     $sql="SELECT * FROM `` WHERE `` = ?";
+
+    $prepare = $co->prepare($sql);
+
+    try{
+        $prepare->execute([$user]);
+    }
 }
