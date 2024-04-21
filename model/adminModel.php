491f488a8c256124211177ba8968ed01bbfa5ec3
BailGui <guillaumebaillieux88@gmail.com>
2024-04-21 20:33:02 +0200

    adModel

--- model/adminModel.php
+++ model/adminModel.php
@@ -10,6 +10,13 @@ function connectAdministrator(PDO $co, string $user, string $password) : bool|st
         $prepare->execute([$user]);
         if($prepare->rowCount()===0) return false;
         $result = $prepare->fetch();
+
+        if(password_verify($password, $result[''])){
+
+            $_SESSION = $result;
+            unset($_SESSION['']);
+            return true;
+        }
     }
 
 catch(Exception $e){
