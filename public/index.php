2e5a8a4ea5b2d53f3b40f54568e439d5bd909bef
BailGui <guillaumebaillieux88@gmail.com>
2024-04-21 20:49:19 +0200

    index

--- public/index.php
+++ public/index.php
@@ -4,3 +4,20 @@ session_stat();
 
 require_once "../config.php";
 require_once "../model/administratorModel.php"
+
+try{
+
+    $db = new PDO(DB_DRIVER . ".host=" . DB_HOST . ";dbname=". DB_NAME . ";charset=" . DB_CHARSET . ";port=" . DB_PORT , DB_LOGIN, DB_PASSWORD, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+
+}catch(Exception $e){
+    die($e->getMessage());
+}
+
+if(isset($_SESSION[''])){
+
+    require_once "../controller/adminControl.php"
+
+}else{
+
+    require_once ".../controller/publicControl.php"
+}
