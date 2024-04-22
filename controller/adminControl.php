1a399b2ad76577e8eb41035c35ffc2d2d76cd292
BailGui <guillaumebaillieux88@gmail.com>
2024-04-22 21:16:35 +0200

    adminControl

--- controller/adminControl.php
+++ controller/adminControl.php
@@ -6,3 +6,17 @@ if(isset($_GET['update'])&&ctype_digit($_GET['update'])){
 
     $id = (int) $_GET['update'];
 
+    if(isset(
+        $_POST['idourdatas'],
+        $_POST['title'],
+        $_POST['ourdesc'],
+        $_POST['latitude'],
+        $_POST['longitude']
+    )){
+        $idourdatas = (int) $_POST['idourdatas'];
+        $title = htmlspecialchars(strip_tags(trim($_POST['title'])),ENT_QUOTES);
+        $ourdesc = htmlspecialchars(trim($_POST['ourdesc']),ENT_QUOTES);
+        $latitude = (float) $_POST['latitude'];
+        $logintude = (float) $_POST['longitude'];
+
+    }
