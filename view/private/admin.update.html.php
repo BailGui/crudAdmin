cc1b9f02562e8776f62d3d3cae34829b5bcab2e7
BailGui <guillaumebaillieux88@gmail.com>
2024-04-23 19:52:37 +0200

    adminUpdate

--- view/private/admin.update.html.php
+++ view/private/admin.update.html.php
@@ -6,6 +6,20 @@
     <title>Admin Update</title>
 </head>
 <body>
+<h1>Admin Update</h1>
+    <nav>
+        <ul>
+            <li><a href="./">Accueil Admin</a></li>
+            <li><a href="?insert">Ajouter une data</a></li>
+            <li><a href="?disconnect">Déconnexion</a>       
+        </ul>
+    </nav>
+    <div class="content">
+        <h2>Modification d'un data</h2>
+        <?php if(isset($error)): ?>
+            <h3 class="error"><?=$error?></h3>
+        <?php endif?>
+    </div>
     
 </body>
 </html>
