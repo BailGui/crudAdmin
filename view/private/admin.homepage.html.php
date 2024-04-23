92e51170362b264ef68f0abb91bdfca3a6dbd61a
BailGui <guillaumebaillieux88@gmail.com>
2024-04-23 10:47:14 +0200

    homepage

---
+++ view/private/admin.homepage.html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Homepage</title>
+</head>
+<body>
+    <h1>Admin Homepage</h1>
+    <nav>
+        <ul>
+            <li><a href="./">Accueil Admin</a></li>
+            <li><a href="?insert">Ajouter une data</a></li>
+            <li><a href="?disconnect">Déconnexion</a>       
+        </ul>
+    </nav>
+    <div class="content">
+        <h2>Admin de nos datas</h2>
+    </div>
+</body>
+</html>
