12a069e782aaa461b44abf549a33d7f69871ae2b
BailGui <guillaumebaillieux88@gmail.com>
2024-04-23 10:01:08 +0200

    publicControl

--- controller/publicControl.php
+++ controller/publicControl.php
@@ -13,9 +13,9 @@ if(isset($_GET['json'])){
 if(isset($_GET['connect'])){
 
     if(isset($_POST['username'],$_POST['passwd'])){
-        $username = htmlspecialchars(strip_tags(trim($_POST[''])),ENT_QUOTES);
-        $userpwd = trim($_POST['']);
-        $connect = connectAdministrator($db,$username,$userpwd);
+        $username = htmlspecialchars(strip_tags(trim($_POST['username'])),ENT_QUOTES);
+        $userpwd = trim($_POST['passwd']);
+        $connect = connectAdministrator($db,$username,$passwd);
 
         if($connect===true){
             header("Location: ./");
@@ -27,5 +27,15 @@ include "../view/public/connect.view.html.php";
 die();
 
 }
+
+if(isset($_GET['json'])){
+    $ouDatas = getAllOurdatas($connect,"ASC");
+    echo json_encode($ourDatas);
+    die(); 
+}
+
+$ourDatas = getAllOurdatas($connect);
+if(is_string($ourDatas)) $message = $ourDatas;
+elseif(isset($ourDatas['error'])) $error = $ourDatas['error'];
 
 include "../view/public/homepage.view.html.php"
