d7cb6164019f0fd539a564fbafeb37ccbe5e98d6
BailGui <guillaumebaillieux88@gmail.com>
2024-04-27 10:15:11 +0200

    navIncAdmin

---
+++ view/inc/navAdmin.php
@@ -0,0 +1,7 @@
+<nav>
+        <ul>
+            <li><a href="?bienvenue">Accueil de l'administration</li>
+            <li><a href="?insert">Ajouter un lieu</a></li>
+            <li><a href="?disconnect">Déconnexion</a></li>
+        </ul>
+</nav>
