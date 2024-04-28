cfcdd170392541a60c8b773e28c9c5fac4f42607
BailGui <guillaumebaillieux88@gmail.com>
2024-04-28 15:46:26 +0200

    majStyle

--- view/private/admin.insert.html.php
+++ view/private/admin.insert.html.php
@@ -14,26 +14,47 @@ include "../view/inc/connect.php";
     <title>Admin insertion</title>
 </head>
 <body>
-    <h1>Admin Insertion</h1>
-    <?php 
+<header>
+        <div class="container">
+        <div class="row mb-5">
+<?php 
     include "../view/inc/navAdmin.php";
     ?>
-    <div class="content">
-        <h2>Insertion d'une data</h2>
+         </div>
+        </div>
+</header>
+<div class="container">
+        <div class="row">
+            <div class="col p-5">
+        <h1 class="text-center">Admin Insertion</h1>
         <?php if(isset($error)): ?>
                 <h3 class="error"><?=$error?></h3>
         <?php endif ?>
-       <form action="" name="ourdatas" method="POST">
-        <input type="text" name="title" placeholder="title" required><br>
-        <textarea name="ourdesc" placeholder="Descrition" required></textarea><br>
-        <input type="number" step="0.0000001" name="latitude" placeholder="latitude" required><br>
-        <input type="number" step="0.0000001" name="longitude" placeholder="longitude" required><br>
-        <input type="submit" value="Insérer" />
-       </form>
+        </div>
+        </div>
+        </div>   
+        <div class="container-md-3">
+        <div class="row formulaire">
+              <div class="col m-5">
+            <form class="d-flex flex-column align-items-center justify-content-center" name="ourdatas" method="POST">
+            <div class="form-group">
+        <input type="text" class="form-control text-center bg-light"  name="title" placeholder="title" required><br>
+        <textarea class="form-control text-center bg-light" name="ourdesc" placeholder="Descrition" required></textarea><br>
+        <input type="number" class="form-control text-center text-alert bg-light" step="0.0000001" name="latitude" placeholder="latitude" required><br>
+        <input type="number" class="form-control text-center text-alert bg-light" step="0.0000001" name="longitude" placeholder="longitude" required><br>
+        </div>
+            <div class="form-group">
+        <input type="submit" class="form-control text-center text-white bg-success" value="Insérer" />
+        </div>
+            </form>
     </div>
+    </div>
+
+<div class="pt-5">
 <?php 
     include "../view/inc/footer.php";
 ?>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
